acc43db2d3b74d92e176697e3442a3a342c1f142
David Cockbain <davidjcockbain@gmail.com>
2024-09-18 15:49:00 +0200

    prep2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.ComponentModel;
 
-class Program
+
+Console.Write("what is your grade percentage? ");
+string input = Console.ReadLine();
+int gradepercent = int.Parse(input);
+
+string gradeletter = "";
+
+if (gradepercent >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    gradeletter = "A"; 
 }
+else if (gradepercent >= 80)
+{
+    gradeletter = "B";
+}
+else if (gradepercent >= 70)
+{
+    gradeletter = "C"; 
+}
+else if (gradepercent >= 60)
+{
+    gradeletter = "D";
+
+}
+else if (gradepercent < 60)
+{
+    gradeletter = "F";
+    
+}
+Console.WriteLine($"your grade is {gradeletter} ");
+if (gradepercent >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Good try you will get it next time!");
+        }
+
+
