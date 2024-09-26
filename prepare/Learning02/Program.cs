30af1f493edbe2307802e383ff1e3f0c5294350e
David Cockbain <davidjcockbain@gmail.com>
2024-09-26 15:56:20 +0200

    fixed errors

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+     Job job1 = new Job();
+     job1._company = "Google";
+     job1._jobTitle = "Software Engineer";
+     job1._startYear = "2024";
+     job1._endYear = "2024";
+     
+
+
+    Job job2 = new Job();
+     job2._company = "github";
+     job2._jobTitle = "Software develoer";
+     job2._startYear = "2022";
+     job2._endYear = "2028";
+
+     Resume myResume = new Resume();
+     myResume._name = "John Smith ";
+
+     myResume._jobs.Add(job1);
+     myResume._jobs.Add(job2);
+
+     myResume.Display();
     }
 }
