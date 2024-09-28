f9e33db672cd7d4b8c2b8a4837c2ce6d5c8218e3
David Cockbain <davidjcockbain@gmail.com>
2024-09-28 23:37:20 +0200

    tried my best

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,49 @@
 using System;
 
 class Program
-{
+{ 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
-    }
+    
+        
+        int user_in = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("1. Wright:");
+    Console.WriteLine("2. Display:");
+    Console.WriteLine("3. save:");
+    Console.WriteLine("4. load:");
+    Console.WriteLine("5. Quit:");
+
+//int user_in = Console.ReadLine;
+
+if (user_in == 1)
+{
+ Prompt prompt = new Prompt();
+    prompt.Display();
 }
+else if (user_in == 2)
+{
+
+}
+else if (user_in == 2)
+{
+
+}
+else if (user_in == 2)
+{
+
+}
+else if (user_in == 2)
+{
+
+}
+       // 
+     
+    }
+    
+}
+
+
+
+
+
